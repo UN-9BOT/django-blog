8b079a9b8d5ff62f4aee86fb162e44e843a0721a
UN-9BOT <un.9bot@gmail.com>
2023-05-16 15:12:51 +0300

    add most commented posts

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from django.db.models import QuerySet
+from django.db.models import QuerySet, Count
 from ..models import Post
 
 register = template.Library()
@@ -14,3 +14,9 @@ def total_posts() -> int:
 def show_latest_posts(count: int = 5) -> dict[str, QuerySet]:
     latest_posts: QuerySet[Post] = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count: int = 5) -> QuerySet[Post]:
+    return Post.published.annotate(
+        total_comments=Count('comments')).order_by('-total_comments')[:count]
