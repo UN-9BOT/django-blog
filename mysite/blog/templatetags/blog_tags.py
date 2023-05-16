dc1a7ff1f5d4db0f0e26e0e5fbea1f20e20a1fe0
UN-9BOT <un.9bot@gmail.com>
2023-05-16 13:34:22 +0300

    realize templates for latest post and count all post

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import QuerySet
 from ..models import Post
 
 register = template.Library()
@@ -7,3 +8,9 @@ register = template.Library()
 @register.simple_tag
 def total_posts() -> int:
     return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count: int = 5) -> dict[str, QuerySet]:
+    latest_posts: QuerySet[Post] = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
